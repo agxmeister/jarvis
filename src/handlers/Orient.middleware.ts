eb69af7534ea03a1bba1ec2386d783a20f564ef9
agxmeister <agxmeister@gmail.com>
2025-05-02 20:51:34 +0300

    Rename the Orient middleware that controls the OODA loop flow to Gatekeeper.

--- src/handlers/Orient.middleware.ts
+++ src/handlers/Orient.middleware.ts
@@ -2,7 +2,7 @@ import {Orientation, State} from "../ooda";
 import {Context} from "../middleware";
 import {OrientationProperties} from "../types";
 
-export const Orient = async (context: Context<State, Orientation<OrientationProperties>>, next: () => Promise<void>): Promise<void> =>
+export const Gatekeeper = async (context: Context<State, Orientation<OrientationProperties>>, next: () => Promise<void>): Promise<void> =>
 {
     if (context.payload.completed) {
         context.state.restart = true;
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,7 +1,7 @@
 export {Preface} from './Preface.handler';
 export {Observe} from './Observe.handler';
 export {Orient as OrientHandler} from './Orient.handler';
-export {Orient as OrientMiddleware} from './Orient.middleware';
+export {Gatekeeper as OrientMiddleware} from './Orient.middleware';
 export {Decide} from './Decide.handler';
 export {Act} from './Act.handler';
 export {Conclude} from './Conclude.handler';
